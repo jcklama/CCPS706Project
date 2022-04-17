f5202b09a2261cd503fe437895031b2d90a78bff
Jacob Lam <Jacob.Lam@capco.com>
2022-04-17 16:18:27 -0400

    v2: final-ish version

--- server.py
+++ server.py
@@ -1,39 +1,17 @@
-# Pseudocode Implementation
-# Infinite Loop that waits for requests
-#   If receive request
-#       check if resource is in cache
-#       if not in cache
-#           make request
-#       else
-#            return the resource from the cache
-
-# should be able to handle requests for HTML pages, images
-
-# Frontend
-# add a few buttons to make a request
-# user input a request
-# should show the amount of time needed to make a request
-# show a list of previous attempts to make the request (number, wURL, round-trip time)
-
 import socket
-# import urllib.request
 import urllib.error
-from bs4 import BeautifulSoup
 import requests
-from requests import adapters
-import json
-import ssl
-from urllib3 import poolmanager
 from urllib.request import Request, urlopen
+import time
 
 HOST = 'localhost'
 PORT = 8001
 BUFFER_SIZE = 1024
+WEBSITE_REGEX = 'www.'
+server_cache = {}
 
-
+# server implementation using sockets
 def main():
-
-    server_cache = {}
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -46,77 +24,70 @@ def main():
         request = client_connection.recv(BUFFER_SIZE).decode()
 
         headers = request.split('\n')
-        search = headers[0].split()[1][1:]
-        print(f'URL received: {search}')
+        if headers:
+            search = headers[0].split()[1][1:]  # TODO: fix to allow browsers that use proxy settings and passing in URLs like this: www.google.com (as opposed to localhost:8001/www.google.com)
+            print(f'URL received: {search}')
 
-        if search in server_cache:
-            response = 'HTTP/1.0 200 OK\n\n' + server_cache[search]
-            client_connection.sendall(response.encode())
-        else:
-            # get resource
-            try:
-                url = 'https://' + search
-                q = Request(url)
-                response = urlopen(q)
+            cache_start = time.time()
+            if search in server_cache:
+                retrieve_from_cache(cache_start, search, client_connection)
+            else:
+                retrieve_from_http(search, client_connection)
 
-                response_headers = response.info()
-                content = response.read().decode('latin-1')
-                # print(content)
-
-                # TODO: add logic for file type (binary, text), retrieving images from site
-                # TODO: error handling for the urllib3.exceptions.MaxRetryError (only make one request?)
-                # response = new_http_response.text
-                # print(response)
-                # To see returned resource's encoding
-                # soup = BeautifulSoup(response, features="html.parser")
-                # print(soup.head.meta)
-
-                # add it to cache
-                if content:
-                    server_cache[search] = content
-                    client_connection.sendall(content.encode())
-
-            # except requests.exceptions.RequestException as e:
-            except urllib.error.URLError as e:
-                print('URLError: {}'.format(e))
-                retry_with_http(search)
-                # if e.code == 404:
-                #     response = 'HTTP/1.0 404 NOT FOUND\n\nFile Not Found'
-                # else:
-                #     response = 'HTTP/1.0 500\n\nCould Not Connect To Server'
+        client_connection.close()
 
 
+def retrieve_from_http(search, client_connection):
+    try:
+        url = 'https://' + search
+        url_start = time.time()
+        content = requests.get(url).text
+
+        if content:
+            url_search_duration = time.time() - url_start
+            server_cache[search] = insert_notification(content, get_notification(True, url_search_duration))
+            client_connection.sendall(('HTTP/1.0 200 OK\n\n' + server_cache[search]).encode())
+
+    except requests.exceptions.RequestException as e:
+        print('URLError: {}'.format(e))
+
+
+def retrieve_from_cache(cache_start, search, client_connection):
+    cache_search_duration = time.time() - cache_start
+    content = 'HTTP/1.0 200 OK\n\n' + server_cache[search]
+    client_connection \
+        .sendall(
+            insert_notification(
+                content,
+                get_notification(False, cache_search_duration)).encode()
+            )
+
+
+def get_notification(init, duration):
+    return f'<div style="text-align:center; color: green; padding-top:15px">' \
+           f'{"Initial" if init else "Cache"} retrieval time: {duration} seconds' \
+           f'</div'
+
+
+def insert_notification(content, notification):
+    body_open = '<body'
+    body_close = '>'
+    start = content.find(body_open)
+    if start != -1:
+        end = content.find(body_close, start)
+        if end != -1:
+            return content[:end+1] + notification + content[end:]
+    return content
+
+# unused
 def retry_with_http(site_name):
     try:
         url = 'http://' + site_name
         q = Request(url)
         response = urlopen(q)
         content = response.read().decode('latin-1')
-        print(content)
     except urllib.error.URLError as e:
         print('URLError: {}'.format(e))
-# Findings
-
-# www.google.com
-# accessing a site often triggers additional requests
-# sometimes a DNS server limits request rates; get a URLError:
-# URLError: <urlopen error [Errno 8] nodename nor servname provided, or not known>
-
-# www.401games.ca
-# URLError: <urlopen error [SSL: SSLV3_ALERT_HANDSHAKE_FAILURE] sslv3 alert handshake failure (_ssl.c:1108)>
-
-# www.walmart.ca
-# got a message asking if I'm a bot (webcrawling)
-
-# Sites that work:
-# www.google.com
-# www.walmart.ca
-
-
-
-# To Run:
-# 1. Run on separate computers
-# 2. Run proxy and browser on same computer
 
 
 if __name__ == '__main__':
